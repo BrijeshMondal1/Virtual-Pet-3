7385948344d25e5d4c61a000fe88feec638712d4
BrijeshMondal1 <brijesh140607@gmail.com>
2021-01-09 11:56:09 +0530

    VIRTUAL PET 2

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var dog, dogImg1, dogImg2, dogSound1, dogSound2, dogSound3;
 var canvas;
 var food;
 var foodS = 20;
-var state = "happy";
+var state = "normal";
 
 function preload(){
 
@@ -19,14 +19,21 @@ function setup() {
   dataBase = firebase.database();
   canvas = createCanvas(windowWidth - 40, windowHeight - 60);
 
-  food = dataBase.ref('foodStock/stock');
-  food.on("value", readStock);
+  food = new Food();
+  dataBase.ref("lastFed").update({time: 0});
 
   dog = createSprite(width/2, height/2);
   dog.scale = 0.5;
   dog.addImage(dogImg1);
 
   dataBase.ref("foodStock").update({stock: 20});
+
+  addFoodButton = createButton("ADD FOOD");
+  addFoodButton.position(width/2 - 80, height - 50);
+  addFoodButton.mousePressed(addFood);
+  feedPetButton = createButton("FEED DOG");
+  feedPetButton.position(width/2 + 20, height - 50);
+  feedPetButton.mousePressed(deductFood);
 
 }
 
@@ -35,12 +42,25 @@ function draw() {
 
   background(46, 139, 87);
 
-  if(keyWentDown(UP_ARROW)){
-    
-    eatFood(foodS);
-    
-    if(foodS > 0){
-    
+  food.display();
+
+  drawSprites();
+
+  fill("red");
+  text("Food remaining : " + foodS, width/2, 50);
+
+}
+
+function deductFood(){
+
+  if(foodS !== 0){
+
+    foodS--;
+    state = "happy";
+    food.updateFoodStock(foodS);
+    food.updateLastFed();
+    console.log("deducting food");
+
       var chooseSound = Math.round(random(1,2));
       
       if(chooseSound === 1)  
@@ -61,96 +81,21 @@ function draw() {
 
         dogSound3.stop();
 
-        }, 1500);
+      }, 1500);
 
     }
-
-    if(state === "happy"){
-      
-      dog.addImage(dogImg2);
-
-    }
-  
-  }else if(keyCode === 32){
-
-    addFood(food);
-
-  }
-
-  drawSprites();
-  
-  textDisplay();
-
-  textAlign(CENTER);
-  textSize(15);
-  textFont("comic sans ms");
-  fill("white");
-  text("Note: Press 'UP ARROW' key to feed Brin! & Press 'SPACE' to add food", width/2, height - 50);
 
 }
-  
-function textDisplay(){
-  
-  if(foodS === 0){
-    
-    fill(245, 245, 220)
-    textSize(25);
-    textAlign(CENTER);
-    text("The food supply has run out!", width/4, height/2);
-    state = "normal";
-    dog.addImage(dogImg1);
 
-  }else if(foodS !== 0){
+function addFood(){
+ 
+  if(foodS < 20){
 
-   state = "happy";
+    foodS++;
+    food.updateFoodStock(foodS);
+    console.log("adding food");
 
   }
 
 }
 
-async function readStock(data){
-  
-  foodS = data.val();
-
-}
-
-function eatFood(y){
-  
-    if(y <= 0){
-      
-      y = 0;
-
-    }else{
-     
-      y--;
-
-    }
-    
-    dataBase.ref("foodStock").update({
-      
-      stock: y
-
-    });
-
-}
-
-function addFood(x){
-  
-  if(x < 20){
-      
-    x += 1;
-
-  }else{
-   
-    x = 20;
-
-  }
-
-  dataBase.ref("foodStock").update({
-    
-    stock: x
-
-  });
-
-
-}
